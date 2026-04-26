6bc2006c84fc6b3298a3f5360d2998dcbefb262a
Riyansha Singh <81138314+riyansha@users.noreply.github.com>
2026-04-27 01:11:41 +0530

    Add: kth-smallest-integer-in-bst - submission-2

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        self.cmt = collections.deque()
+
+        def dfs(node):
+            if not node:
+                return
+            
+            dfs(node.left)
+            self.cmt.append(node.val)   #stats appendinf from leaf
+            if len(self.cmt) == k:
+                return
+            dfs(node.right)
+
+        dfs(root)
+        return self.cmt[k-1]
