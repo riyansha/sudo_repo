974ebd6b2392212c5d9ea67d31fedceb62ce4a08
Riyansha Singh <81138314+riyansha@users.noreply.github.com>
2026-04-27 00:24:57 +0530

    Add: valid-binary-search-tree - submission-5

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-5.py
@@ -0,0 +1,28 @@
+# class Solution:
+#     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+#         def preOrder(node, min_val, max_val):
+#             if not node:
+#                 return True
+#             else:
+#                 if not (min_val < node.val < max_val):
+#                     return False
+#                 return preOrder(node.left, min_val, node.val) and preOrder(node.right, node.val, max_val)
+        
+#         return preOrder(root, float('-inf'), float('inf'))
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        
+        def dfs(node, low, high):
+            if not node:
+                return True
+            
+            if not (low < node.val < high):
+                return False
+            
+            return (
+                dfs(node.left, low, node.val) and
+                dfs(node.right, node.val, high)
+            )
+        
+        return dfs(root, float("-inf"), float("inf"))
